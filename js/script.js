3996fcedbc3043650efc96df2238edb8b5c7eab6
Tomasz Krzemiński <Tomasz.krzeminski4@gmail.com>
2023-11-28 13:51:20 +0100

    empty spaces deleted

--- js/script.js
+++ js/script.js
@@ -13,19 +13,14 @@
         switch (day) {
             case "Poniedziałek":
                 return howMuch.innerText = "5";
-
             case "Wtorek":
                 return howMuch.innerText = "4";
-
             case "Środa":
                 return howMuch.innerText = "3";
-
             case "Czwartek":
                 return howMuch.innerText = "2";
-
             case "Piątek":
                 return howMuch.innerText = "1";
-
             case "Sobota":
                 return howMuch.innerText = "Mamy weekend!";
             case "Niedziela":
