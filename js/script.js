bb25393e2fd355f62e68b5c531e4d4b2c31f0e85
Tomasz Krzemiński <Tomasz.krzeminski4@gmail.com>
2023-11-28 13:41:26 +0100

    ES6+ features added to function

--- js/script.js
+++ js/script.js
@@ -1,34 +1,38 @@
-console.log("Dzień dobry witam Cię na mojej stronie!");
+{
+    const welcome = () => {
+        console.log("Dzień dobry witam Cię na mojej stronie!");
+    }
 
-let day = document.querySelector(".day").innerText;
-let howMuch = document.querySelector(".howMuch");
+    welcome();
 
-switch (day) {
-    case "Poniedziałek":
-        howMuch.innerText = "5";
-        break;
 
-    case "Wtorek":
-        howMuch.innerText = "4";
-        break;
+    const setHowMuch = () => {
+        const howMuch = document.querySelector(".howMuch");
+        const day = document.querySelector(".day").innerText;
 
-    case "Środa":
-        howMuch.innerText = "3";
-        break;
+        switch (day) {
+            case "Poniedziałek":
+                howMuch.innerText = "5";
+                break;
+            case "Wtorek":
+                howMuch.innerText = "4";
+                break;
+            case "Środa":
+                howMuch.innerText = "3";
+                break;
+            case "Czwartek":
+                howMuch.innerText = "2";
+                break;
+            case "Piątek":
+                howMuch.innerText = "1";
+                break;
+            case "Sobota":
+            case "Niedziela":
+                howMuch.innerText = "Mamy weekend!";
+                break;
 
-    case "Czwartek":
-        howMuch.innerText = "2";
-        break;
+        }
+    }
 
-    case "Piątek":
-        howMuch.innerText = "1";
-        break;
-
-    case "Sobota":
-        howMuch.innerText = "Mamy weekend!";
-        break;
-
-    case "Niedziela":
-        howMuch.innerText = "Mamy weekend!";
-        break;
+    setHowMuch();
 }
